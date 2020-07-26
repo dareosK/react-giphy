7abba1f8ddec6e6143704f7a470bd6530016aa04
Dareos Khalili <khalilidareos@gmail.com>
2020-07-26 18:55:14 +0200

    all functionalities finalised

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,34 +5,47 @@ import Gif from './gif'
 import GifList from './gif_list'
 
 // const giphy = require('giphy-api')('LXDTRWUITSEkVccvdmwScrwjmjxUXiZY');
-// let giflist = []
 
 class App extends Component { // creates the view for the app
   constructor(props) {
     super(props);
 
     this.state = {
-
-    }
+      gifs: [],
+      selectedGifID: "buz1nMlNHqIdG"
+    };
+    this.search('aliens');
   }
 
+  search = (query) => {
+    giphy('LXDTRWUITSEkVccvdmwScrwjmjxUXiZY').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (err, result) => { // ! Doc uses function(err, result), however
+    this.setState({       // 'this' would refer to the function if not turned into arrow function
+      gifs: result.data   // and we need 'this' to refer to the state !
+        });
+      });
+  }
 
+  selectGif = (id) => {
+    this.setState({
+      selectedGifID: id
+    });
+  }
+ // lines 42 and 48 -> pass functions to the respective children
   render () {
-    const gifs = [
-    { id: "buz1nMlNHqIdG" },
-    { id: "hpN4l9ygZHVUk" }
-    ];
-
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search}/>
           <div className="selected-gif">
-            <Gif id="buz1nMlNHqIdG"/>
+            <Gif id={this.state.selectedGifID}/>
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs} />
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif}/>
         </div>
       </div>
     );
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
 class Gif extends Component {
+  handleClick = () => {
+    if (this.props.selectGif) {
+      this.props.selectGif(this.props.id);
+    }
+  }
+
   render() {
-    // id = "buz1nMlNHqIdG"
     const src = `https://media0.giphy.com/media/${this.props.id}/giphy.gif`
     return (
-      <img src={src} className="gif" />
+      <img src={src} className="gif" onClick={this.handleClick}/>
       )
   };
 }
